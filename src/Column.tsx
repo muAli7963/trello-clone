0542f7fd39080ca855d346cb29f030645abf2985
mu ali <elshrab90.ma@gamil.com>
2021-10-18 17:31:10 +0200

    finished

--- src/Column.tsx
+++ src/Column.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { AddNewItem } from "./AddNewItem";
 import { useAppState } from "./state/AppStateContext";
 import { useDrop } from "react-dnd";
-import { moveList, addTask } from "./state/actions";
+import { addTask, moveTask, moveList, setDraggedItem } from "./state/actions";
 import { useItemDrag } from "./utils/useItemDrag";
 import { isHidden } from "./utils/isHidden";
 import { Card } from "./Card";
@@ -19,7 +19,7 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
   const tasks = getTaskByListId(id);
   const ref = useRef<HTMLDivElement>(null);
   const [, drop] = useDrop({
-    accept: "COLUMN",
+    accept: ["COLUMN", "CARD"],
     hover() {
       if (!draggedItem) {
         return;
@@ -29,6 +29,15 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
           return;
         }
         dispatch(moveList(draggedItem.id, id));
+      } else {
+        if (draggedItem.columnId === id) {
+          return;
+        }
+        if (tasks.length) {
+          return;
+        }
+        dispatch(moveTask(draggedItem.id, null, draggedItem.columnId, id));
+        dispatch(setDraggedItem({ ...draggedItem, columnId: id }));
       }
     },
   });
@@ -43,7 +52,7 @@ export const Column = ({ text, id, isPreview }: ColumnProps) => {
     >
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
-        <Card text={task.text} key={task.id} id={task.id} />
+        <Card id={task.id} columnId={id} text={task.text} key={task.id} />
       ))}
       <AddNewItem
         toggleButtonText="+ Add another task"
--- src/DragItem.ts
+++ src/DragItem.ts
@@ -1,6 +1,12 @@
+export type CardDragItem = {
+  id: string;
+  columnId: string;
+  text: string;
+  type: "CARD";
+};
 export type ColumnDragItem = {
   id: string;
   text: string;
   type: "COLUMN";
 };
-export type DragItem = ColumnDragItem;
+export type DragItem = CardDragItem | ColumnDragItem;
